a8a9e65e17b3650c9d23bb4a9edbb488b4452f69
mika_sampaio <mikaasampaio6@gmail.com>
2025-03-30 16:50:00 -0300

    feat: tela de produtos favoritados

--- src/pages/favoritos.tsx
+++ src/pages/favoritos.tsx
@@ -5,16 +5,20 @@ import { Product } from "@/services/products";
 import { UserService } from "@/services/user";
 import { Box, Center, Heading, HStack, Text, VStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { AiOutlineSearch } from "react-icons/ai";
 
 export default function Favoritos() {
   const [favorites, setFavorites] = useState<Product[]>([]);
+  const methods = useForm();
+  const { watch } = methods;
 
   const getFavorites = async () => {
     try {
-      const response = await UserService.getFavorites();
+      const response = await UserService.getFavorites({
+        search: watch("search"),
+      });
 
-      console.log(response);
       setFavorites(response);
     } catch (error) {
       console.error(error);
@@ -22,60 +26,67 @@ export default function Favoritos() {
   };
 
   useEffect(() => {
-    getFavorites();
-  }, []);
+    const getFavoriteDebounce = setTimeout(() => {
+      getFavorites();
+    }, 500);
+
+    return () => clearTimeout(getFavoriteDebounce);
+  }, [watch("search")]);
 
   return (
     <Authenticated>
-      <Box minH="calc(100vh - 65px)" bg="gray.secondary" p="2.5rem">
-        <VStack
-          alignItems="flex-start"
-          bg="white"
-          borderRadius=".5rem"
-          p="1.5rem"
-          h="100%"
-          gap="1.5rem"
-        >
-          <HStack justifyContent="space-between" w="100%">
-            <Heading as="h1" size="md">
-              Favoritos
-            </Heading>
-
-            <Box>
-              <InputText
-                name="searchFavorite"
-                label=""
-                placeholder="Pesquisar..."
-                rightIcon={<AiOutlineSearch fontSize="1.5rem" />}
-              />
-            </Box>
-          </HStack>
-
-          <HStack
+      <FormProvider {...methods}>
+        <Box minH="calc(100vh - 65px)" bg="gray.secondary" p="2.5rem">
+          <VStack
+            alignItems="flex-start"
+            bg="white"
+            borderRadius=".5rem"
+            p="1.5rem"
             h="100%"
-            w="100%"
-            gap="2rem"
-            justifyContent={{ sm: "space-between", md: "initial" }}
-            flexWrap="wrap"
+            gap="1.5rem"
           >
-            {favorites.length > 0 ? (
-              favorites.map((product) => (
-                <FavoriteProducts
-                  key={product._id}
-                  product={product}
-                  isFavorited
+            <HStack justifyContent="space-between" w="100%">
+              <Heading as="h1" size="md">
+                Favoritos
+              </Heading>
+
+              <Box>
+                <InputText
+                  name="search"
+                  label=""
+                  placeholder="Pesquisar..."
+                  rightIcon={<AiOutlineSearch fontSize="1.5rem" />}
                 />
-              ))
-            ) : (
-              <Center w="100%">
-                <Text size="sm" color="gray.400" fontWeight="300">
-                  Nenhum produto favoritado.
-                </Text>
-              </Center>
-            )}
-          </HStack>
-        </VStack>
-      </Box>
+              </Box>
+            </HStack>
+
+            <HStack
+              h="100%"
+              w="100%"
+              gap="2rem"
+              justifyContent={{ sm: "space-between", md: "initial" }}
+              flexWrap="wrap"
+            >
+              {favorites.length > 0 ? (
+                favorites.map((product) => (
+                  <FavoriteProducts
+                    key={product._id}
+                    product={product}
+                    isFavorited
+                    refresh={getFavorites}
+                  />
+                ))
+              ) : (
+                <Center w="100%">
+                  <Text size="sm" color="gray.400" fontWeight="300">
+                    Nenhum produto favoritado.
+                  </Text>
+                </Center>
+              )}
+            </HStack>
+          </VStack>
+        </Box>
+      </FormProvider>
     </Authenticated>
   );
 }
